fea6448ad57bc98d15dc15b62f9858a77706b87c
chris <chris.nooteboom@gmail.com>
2024-08-01 15:23:10 +0200

    added calender main page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <CalenderMainpage/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalenderMainpage from './components/CalenderMainpage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalenderMainpage
   }
 }
 </script>
